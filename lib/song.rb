4744a52759337fc3b4939cc9e1c92474d85b5a41
Gary Wheeler <gkwheeler40@gmail.com>
2019-05-16 15:05:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,9 +2,20 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0 
+  @@genres = []
   
   def initialize(song_name, artist, genre)
     @name = song_name
     @artist = artist
+    @genre = genre
+    @@count += 1 
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.genres
+    
   end
 end
