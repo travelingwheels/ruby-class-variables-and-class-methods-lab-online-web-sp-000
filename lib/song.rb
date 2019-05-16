75f6640f44ffe21e3a3058c93b9f0cf8b2257d63
Gary Wheeler <gkwheeler40@gmail.com>
2019-05-16 15:40:34 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -35,7 +35,7 @@ class Song
   
   def self.artist_count
     artist_count = Hash.new(0)
-    @@artist.each {|artist| artist_count[artist] += 1}
+    @@artists.each {|artist| artist_count[artist] += 1}
     artist_count
   end
 end
